935446cc4f5013593f38cd1ff05ec3cf471c23c8
Wendivoid <wendivoid@gmail.com>
2024-07-08 00:14:42 -0500

    Remove Sickle UI from plant example & add FPS overlay

--- crates/plants/examples/plant.rs
+++ crates/plants/examples/plant.rs
@@ -1,13 +1,8 @@
 use bevy::prelude::*;
 
-mod utils;
-
-use plants::{
-    material::PlantMaterial,
-    PlantBundle,
-    mesh::MeshRenderer,
-};
-use sickle_ui::prelude::Dropdown;
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+use bevy_pbr::wireframe::WireframePlugin;
+use plants::{material::PlantMaterial, mesh::MeshRenderer, PlantBundle};
 
 #[derive(Component)]
 pub struct Plant {
@@ -15,17 +10,23 @@ pub struct Plant {
     pub cfg: plants::mesh::MeshRenderConfig,
 }
 
-
 fn main() {
     App::new()
-        .add_plugins(utils::ExamplePlugin)
-        .add_systems(Startup, setup)
-        .add_systems(Update, (mesh_lsystem, lsystem_changed))
+        .add_plugins((
+            DefaultPlugins,
+            PanOrbitCameraPlugin,
+            plants::PlantPlugin,
+            WireframePlugin,
+            bevy::dev_tools::fps_overlay::FpsOverlayPlugin::default()
+        ))
+        .add_systems(Startup, (setup, setup_world))
+        .add_systems(Update, mesh_lsystem)
         .run();
 }
 
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<PlantMaterial>>) {
-    commands.spawn(PlantBundle {
+    commands
+        .spawn(PlantBundle {
             material: materials.add(PlantMaterial { time_scale: 1.0 }),
             ..Default::default()
         })
@@ -33,6 +34,35 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<PlantMaterial>>) {
             lsystem: plants::monopodial(),
             cfg: Default::default(),
         });
+}
+
+fn setup_world(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Circle::new(8.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        ..default()
+    });
+
+    commands
+        .spawn(Camera3dBundle {
+            transform: Transform::from_xyz(10.0, 10.0, 10.0),
+            ..default()
+        })
+        .insert(PanOrbitCamera::default());
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
 }
 
 fn mesh_lsystem(
@@ -43,32 +73,5 @@ fn mesh_lsystem(
     for (entity, plant) in query.iter() {
         let mesh = MeshRenderer::new(&plant.lsystem, plant.cfg.clone()).build();
         commands.entity(entity).insert(meshes.add(mesh));
-    }
-}
-
-fn lsystem_changed(
-    mut commands: Commands,
-    plant_entity: Query<Entity, With<Plant>>,
-    query: Query<&Dropdown, Changed<Dropdown>>,
-) {
-    for dropdown in query.iter() {
-        if let Some(value) = dropdown.value() {
-            let plant = match value {
-                0 => Plant {
-                    lsystem: plants::monopodial(),
-                    cfg: Default::default(),
-                },
-                1 => Plant {
-                    lsystem: plants::sympodial(),
-                    cfg: Default::default(),
-                },
-                2 => Plant {
-                    lsystem: plants::ternary(),
-                    cfg: Default::default(),
-                },
-                _ => unreachable!(),
-            };
-            commands.entity(plant_entity.single()).insert(plant);
-        }
     }
 }
